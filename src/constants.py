35e0ed2167b4d499c0cb0b46d78386cb1b8c2413
ayomodu <ayomodu@gmail.com>
2024-11-30 17:52:51 -0500

    initial push

---
+++ src/constants.py
@@ -0,0 +1,57 @@
+PROJ_DATA = {
+                    "data": {
+                    "type": "projects",
+                    "attributes": {
+                        "name": "",
+                        "description": ""
+                        }
+                    }
+            }
+
+WORKSPACE_DATA = {
+                    "data": {
+                        "type": "workspaces",
+                        "attributes": {
+                            "name": "",
+                            "execution-mode": "",
+                            "description": "",
+                            "working-directory": "",
+                            "source-name" : "terrk"
+                        }
+
+                    }
+                }
+
+HEADERS = {
+            "Authorization": "",
+            "Content-Type": "application/vnd.api+json"
+          }
+
+VCS_CONFIG = {
+    "tags-regex": "",
+    "oauth-token-id": "",
+    "ingress-submodules": "",
+    "identifier": "",
+    "branch": ""
+}
+
+AGENT_POOL_DATA = {
+    "data": {
+        "type": "agent-pools",
+        "attributes": {
+            "name": "",
+            "organization-scoped": False,
+        }
+    }
+}
+
+WORKSPACE_PROJECT_REL =  {
+                        "project": {
+                            "data": {
+                                "type": "projects",
+                                "id": ""
+                            }
+                        }
+                }
+
+HTTP_SUCCESS_CODES = {200,201,202,203,204}
